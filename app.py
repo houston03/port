640425028005ae8b9fe79e4896ebb6706871d1a5
houston03 <152857475+houston03@users.noreply.github.com>
2024-09-23 14:06:50 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/project')
+def project():
+    return render_template('project.html')
+
+
+
